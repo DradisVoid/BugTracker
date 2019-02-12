ea01f62ce55df792b6d1249bee9e81cf9904503c
Daniel Arn <danielarn1799@gmail.com>
2019-02-12 11:59:46 -0500

    Initial commit

---
+++ app/src/main/java/edu/andrews/cptr252/arn/bugtracker/BugTrackerActivity.java
@@ -0,0 +1,13 @@
+package edu.andrews.cptr252.arn.bugtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BugTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bugtracker);
+    }
+}
